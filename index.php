4218dd1a8c794d58f0922b531c8b434638f28101
Asrafuzzaman01 <asrafuzzaman.cse.siu@gmail.cvom>
2022-08-09 23:44:57 +0600

    variable dec...and two number add

--- index.php
+++ index.php
@@ -4,15 +4,39 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Basic Php practice tutorial </title>
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h2>Basic Php practice tutorial </h2>
 
-    <?php
+      <section class="header">
+       <?php
 
-echo "hello php";
+echo " <h2>Basic Php practice tutorial </h2>";
+
+//variable declaration 
+$x = 5;
+$y = 6;
+$z = $x + $y;
+echo "$z";
+
+
+
+
+
+
+
 ?>
+      </section>
+
+
+  
+   
+   <section class="footer">
+    
+   </section>
+
 
     
 </body>
