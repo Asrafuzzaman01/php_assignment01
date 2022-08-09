bcda3953b0823407a880802e7bed393d126daf31
Asrafuzzaman01 <asrafuzzaman.cse.siu@gmail.cvom>
2022-08-10 01:46:54 +0600

    number file added

---
+++ number.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>number calculation and check </title>
+</head>
+<body>
+
+<h2>basic php tutorial practice </h2>
+<?php
+//Check if the type of a variable is integer:
+$x = 66;
+var_dump(is_int($x));
+$v = 12.78;
+var_dump(is_int($v));
+
+
+?>
+
+    
+</body>
+</html>
