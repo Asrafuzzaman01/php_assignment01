7f035c44ccb75c0b777e0755e84bcf9b8a6d28b4
Asrafuzzaman01 <asrafuzzaman.cse.siu@gmail.cvom>
2022-08-10 21:09:12 +0600

    control statement

---
+++ control_statement.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>control statement</title>
+
+</head>
+<body>
+
+<h2>COntrol statement</h2>
+<?php
+$numbr = 66;
+if ($numbr < 100) {
+    echo "$numbr is less than 100";
+}
+
+
+
+
+
+
+
+
+?>
+    
+</body>
+</html>
